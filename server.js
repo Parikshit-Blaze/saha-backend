fd05ee689f2b8b642e330df96d97dd7dd5ed9bd3
Parikshit-Blaze <parikshitsaha1997@gmail.com>
2026-05-27 21:58:34 -0700

    Add files via upload

--- server.js
+++ server.js
@@ -13,12 +13,15 @@ app.post("/api/orders", async (req, res) => {
     const order = req.body;
     console.log("Order received:", order);
 
-    const transporter = nodemailer.createTransport({
-      service: "gmail",
+  const transporter = nodemailer.createTransport({
+      host: "smtp.gmail.com",
+      port: 465,
+      secure: true,
       auth: {
         user: process.env.EMAIL_USER,
         pass: process.env.EMAIL_PASS,
       },
+      connectionTimeout: 30000,
     });
 
     const mailOptions = {
